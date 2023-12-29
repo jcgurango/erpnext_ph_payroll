9da9c8f87ada083f898f88cdef38661cf0275626
iaiaian1 <yapjakebrian1@gmail.com>
2023-12-29 13:53:25 +0800

    BIR 1601-C Doctype and Print Format

---
+++ erpnext_ph_payroll/erpnext_ph_payroll/doctype/bir_form_1601_c/bir_form_1601_c.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, JC Gurango and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class BIRForm1601C(Document):
+	pass
---
+++ erpnext_ph_payroll/erpnext_ph_payroll/doctype/bir_form_1601_c/test_bir_form_1601_c.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, JC Gurango and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBIRForm1601C(FrappeTestCase):
+	pass
